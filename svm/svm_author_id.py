e915716a28a23bd776f4f554d2537aa4c6c09f78
Gerardo Teruel <gerardogtn@gmail.com>
2015-05-29 16:42:58 -0500

    Add svc algorithm, tested and with time indicators

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python
 
-""" 
+"""
     this is the code to accompany the Lesson 2 (SVM) mini-project
 
     use an SVM to identify emails from the Enron corpus by their authors
-    
+
     Sara has label 0
     Chris has label 1
 
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+import numpy as np
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
 
 
 ### features_train and features_test are the features for the training
@@ -26,7 +29,18 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+clfr = SVC(kernel= "linear")
+
+t0 = time()
+clfr.fit(features_train, labels_train)
+print "training time: ", round(time() - t0, 3), "s"
+
+t1 = time()
+pred = clfr.predict(features_test)
+print "predicting time: ", round(time() - t1, 3), "s"
+
+acc = accuracy_score(pred, labels_test)
+
+print acc
 
 #########################################################
-
-
