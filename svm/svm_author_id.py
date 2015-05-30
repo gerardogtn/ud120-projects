e9c8ecca43515937757dead6359b675a165c5cff
Gerardo Teruel <gerardogtn@gmail.com>
2015-05-30 16:57:47 -0500

    Use only 1% of training data

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -24,7 +24,9 @@ from sklearn.metrics import accuracy_score
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
+## Smaller data fit.
+features_train = features_train[:len(features_train)/100]
+labels_train = labels_train[:len(labels_train)/100] 
 
 
 #########################################################
