25dc2f562cd6ca14dda0871f1fc252b870687404
Gerardo Teruel <gerardogtn@gmail.com>
2015-05-30 17:21:09 -0500

    Train all data

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -25,8 +25,8 @@ from sklearn.metrics import accuracy_score
 features_train, features_test, labels_train, labels_test = preprocess()
 
 ## Smaller data fit.
-features_train = features_train[:len(features_train)/100]
-labels_train = labels_train[:len(labels_train)/100]
+##features_train = features_train[:len(features_train)/100]
+##labels_train = labels_train[:len(labels_train)/100]
 
 
 #########################################################
