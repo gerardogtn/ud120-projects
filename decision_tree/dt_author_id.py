7134e8c0f5986d06e0179390713ea0fe618942dd
Gerardo Teruel <gerardogtn@gmail.com>
2015-05-30 21:45:19 -0500

    Add print number of features option

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -22,21 +22,21 @@ from sklearn import tree
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
+print len(features_train[0])
 
 
 #########################################################
 ### your code goes here ###
-clfr = tree.DecisionTreeClassifier(min_samples_split=40)
+##clfr = tree.DecisionTreeClassifier(min_samples_split=40)
 
-t0 = time()
-clfr.fit(features_train, labels_train)
-print "The training time is: ", round(time() - t0, 4), "s"
+##t0 = time()
+##clfr.fit(features_train, labels_train)
+##print "The training time is: ", round(time() - t0, 4), "s"
 
-t1 = time()
-pred = clfr.predict(features_test)
-print "The predicting time is: ", round(time() - t1, 4), "s"
+##t1 = time()
+##pred = clfr.predict(features_test)
+##print "The predicting time is: ", round(time() - t1, 4), "s"
 
-acc = accuracy_score(pred, labels_test)
-print "The accuracy is: ", round(acc, 4)
+##acc = accuracy_score(pred, labels_test)
+##print "The accuracy is: ", round(acc, 4)
 #########################################################
