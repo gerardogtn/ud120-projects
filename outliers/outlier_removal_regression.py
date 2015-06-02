226c769f32193eabcb85d6e5ba9369fdda0eee17
Gerardo Teruel <gerardogtn@gmail.com>
2015-06-01 22:53:02 -0500

    Plot regression and print regression values

--- outliers/outlier_removal_regression.py
+++ outliers/outlier_removal_regression.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import pickle
 
 from outlier_cleaner import outlierCleaner
+from sklearn import linear_model
 
 
 ### load up some practice data with outliers in it
@@ -26,10 +27,13 @@ ages_train, ages_test, net_worths_train, net_worths_test = train_test_split(ages
 ### fill in a regression here!  Name the regression object reg so that
 ### the plotting code below works, and you can see what your regression looks like
 
+reg = linear_model.LinearRegression()
+reg.fit(ages_train, net_worths_train)
 
-
-
-
+print "The slope of the regression is: ", reg.coef_[0]
+print "The intercept of the regression is: ", reg.intercept_
+print "The r-squared value of the training values is: ", reg.score(ages_train, net_worths_train)
+print "The r-squared value of the testing values is: ", reg.score(ages_test, net_worths_test)
 
 
 
@@ -81,4 +85,3 @@ if len(cleaned_data) > 0:
 
 else:
     print "outlierCleaner() is returning an empty list, no refitting to be done"
-
