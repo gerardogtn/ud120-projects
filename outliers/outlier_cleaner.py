bda40c3274a6be8ba8ede5d4a3511d381fe57b88
Gerardo Teruel <gerardogtn@gmail.com>
2015-06-01 23:29:41 -0500

    Implement outlier-cleaner algorithm

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -7,14 +7,21 @@ def outlierCleaner(predictions, ages, net_worths):
         residual errors (different between the prediction
         and the actual net worth)
 
-        return a list of tuples named cleaned_data where 
+        return a list of tuples named cleaned_data where
         each tuple is of the form (age, net_worth, error)
     """
-    
     cleaned_data = []
+    for predict, age, net_worth in zip(predictions, ages, net_worths):
+        cleaned_data.append((predict, age, net_worth, (predict-net_worth)**2))
 
-    ### your code goes here
+    sorted_cleaned_data =  sorted(cleaned_data, key=lambda tup: tup[3])
 
-    
+    i = len(predictions) - 1
+    lastComparison = i - (i / 10)
+    cleaned_data = []
+    for entry in sorted_cleaned_data:
+        if (i < lastComparison):
+            cleaned_data.append(entry[0:3])
+        i = i - 1
+
     return cleaned_data
-
