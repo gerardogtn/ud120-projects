a7d048c4d8657bb78c2aa78be8679cb83eeda081
Gerardo Teruel <gerardogtn@gmail.com>
2015-06-02 23:12:58 -0500

    Add MaxMinScaler

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -14,6 +14,7 @@ import matplotlib.pyplot as plt
 import sys
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
+from sklearn import preprocessing
 
 
 
@@ -53,7 +54,7 @@ feature_1 = "salary"
 feature_2 = "exercised_stock_options"
 feature_3 = "total_payments"
 poi  = "poi"
-features_list = [poi, feature_1, feature_2, feature_3]
+features_list = [poi, feature_1, feature_2]
 data = featureFormat(data_dict, features_list)
 poi, finance_features = targetFeatureSplit(data)
 
@@ -62,7 +63,7 @@ poi, finance_features = targetFeatureSplit(data)
 ### you'll want to change this line to
 ### for f1, f2, _ in finance_features:
 ### (as it's currently written, line below assumes 2 features)
-for f1, f2, f3 in finance_features:
+for f1, f2 in finance_features:
     plt.scatter(f1, f2)
 plt.title("Scatter points")
 plt.show()
@@ -81,7 +82,9 @@ Draw(pred, finance_features, poi, name="clusters_before_scaling.pdf", f1_name=fe
 
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
-clfr = KMeans(n_clusters= 3)
+scaler = preprocessing.MinMaxScaler()
+finance_features = scaler.fit_transform(finance_features)
+clfr = KMeans(n_clusters= 2)
 pred = clfr.fit_predict(finance_features)
 try:
     Draw(pred, finance_features, poi, mark_poi=False, name="clusters.pdf", f1_name=feature_1, f2_name=feature_2)
