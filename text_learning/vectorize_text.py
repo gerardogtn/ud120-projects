d2f98c750fa27968f73dd484f3dd2c82597b6f63
Gerardo Teruel <gerardogtn@gmail.com>
2015-06-04 13:24:12 -0500

    Add email parser with stemming. Add remove keywords from parsedText. Add append strings to word_data and its author to from_data

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -47,14 +47,17 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
             email = open(path, "r")
 
             ### use parseOutText to extract the text from the opened email
-
+            parsedText = parseOutText(email)
             ### use str.replace() to remove any instances of the words
             ### ["sara", "shackleton", "chris", "germani"]
-
+            parsedText.replace("sara ", "")
+            parsedText.replace("shackleton ", "")
+            parsedText.replace("chris ", "")
+            parsedText.replace("germani ", "")
             ### append the text to word_data
-
+            word_data.append(parsedText)
             ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
-
+            from_data.append(0) if (from_person == "sara") else from_data.append(1)
 
             email.close()
 
@@ -70,5 +73,3 @@ pickle.dump( from_data, open("your_email_authors.pkl", "w") )
 
 
 ### in Part 4, do TfIdf vectorization here
-
-
