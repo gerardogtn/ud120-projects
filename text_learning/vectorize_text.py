d9fb5416f23304340d5d4f4c6b3569a1cdf9112d
Gerardo Teruel <gerardogtn@gmail.com>
2015-06-06 22:54:32 -0500

    Fixed from_data appending bug: appending int rather than string

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -8,7 +8,6 @@ import sys
 sys.path.append( "../tools/" )
 from parse_out_email_text import parseOutText
 from sklearn.feature_extraction.text import TfidfVectorizer
-from nltk.corpus import stopwords
 
 """
     starter code to process the emails from Sara and Chris to extract
@@ -43,25 +42,25 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
         ### only look at first 200 emails when developing
         ### once everything is working, remove this line to run over full dataset
         #temp_counter += 1
-        if temp_counter < 50:
-            path = os.path.join('..', path[:-1])
-            print path
-            email = open(path, "r")
+        path = os.path.join('..', path[:-1])
+        print path
+        email = open(path, "r")
 
-            ### use parseOutText to extract the text from the opened email
-            parsedText = parseOutText(email)
-            ### use str.replace() to remove any instances of the words
-            ### ["sara", "shackleton", "chris", "germani"]
-            parsedText = parsedText.replace("sara", "")
-            parsedText = parsedText.replace("shackleton", "")
-            parsedText = parsedText.replace("chris", "")
-            parsedText = parsedText.replace("germani", "")
-            ### append the text to word_data
-            word_data.append(parsedText)
-            ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
-            from_data.append(0) if (from_person == "sara") else from_data.append(1)
+        ### use parseOutText to extract the text from the opened email
+        parsedText = parseOutText(email)
+        ### use str.replace() to remove any instances of the words
+        ### ["sara", "shackleton", "chris", "germani"]
+        parsedText = parsedText.replace("sara", "")
+        parsedText = parsedText.replace("shackleton", "")
+        parsedText = parsedText.replace("chris", "")
+        parsedText = parsedText.replace("germani", "")
+        ### append the text to word_data
+        word_data.append(parsedText)
+        ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
+        from_data.append("0") if (name == "sara") else from_data.append("1")
 
-            email.close()
+        email.close()
+
 
 print "emails processed"
 from_sara.close()
@@ -70,29 +69,6 @@ from_chris.close()
 pickle.dump( word_data, open("your_word_data.pkl", "w") )
 pickle.dump( from_data, open("your_email_authors.pkl", "w") )
 
-
-## Stopwords
-sw = stopwords.words("english")
-
-## String -> Bool
-## Return true if word is an element of sw. False otherwise.
-def isStopWord(word):
-    for stopword in sw:
-        if (stopword == word):
-            return True
-
-    return False
-
-## Filter word_data for stopwords.
-#word_data_filtered = []
-#for words in word_data:
-#    entry = ""
-#
-#    for word in words.split():
-#        if (not isStopWord(word)):
-#            entry = entry + word + " "
-#
-#    word_data_filtered.append(entry)
 
 ### in Part 4, do TfIdf vectorization here
 tfidf = TfidfVectorizer(stop_words="english")
